f913c7a3c3f03ea07af5e4daa71f2bca3643f294
Клёп Дмитрий <dmitiykloyp@gmail.com>
2021-10-13 00:03:26 +0300

    доюавил текстэрию в message

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,44 +1,76 @@
-import {rerenderEntireTree} from "../render";
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 
-let state = {
-  profilePage: {
-    posts: [
-      {id: 1, message: 'hello world!', likeCount: 12},
-      {id: 2, message: 'hi', likeCount: 55},
-      {id: 3, message: 'hello russion!', likeCount: 0},
-    ],
-    newPostText: 'Dima',
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {id: 1, message: 'hello world!', likeCount: 12},
+        {id: 2, message: 'hi', likeCount: 55},
+        {id: 3, message: 'hello russion!', likeCount: 0},
+      ],
+      newPostText: 'Dima',
+    },
+    dialogsPage: {
+      messages: [
+        {id: 1, message: 'hi'},
+        {id: 2, message: 'hello'},
+        {id: 3, message: 'bay'},
+      ],
+      dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Katy'},
+        {id: 3, name: 'Andrey'},
+        {id: 4, name: 'Nasty'},
+        {id: 5, name: 'Oleg'},
+      ],
+      newMessageBody: ''
+    },
+    sidebar: {}
   },
-  dialogsPage: {
-    messages: [
-      {id: 1, message: 'hi'},
-      {id: 2, message: 'hello'},
-      {id: 3, message: 'bay'},
-    ],
-    dialogs: [
-      {id: 1, name: 'Dimych'},
-      {id: 2, name: 'Katy'},
-      {id: 3, name: 'Andrey'},
-      {id: 4, name: 'Nasty'},
-      {id: 5, name: 'Oleg'},
-    ],
+  _callSubscriber() {
+    console.log('state changed');
   },
+
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+
+  dispatch(action) {
+    if (action.type === ADD_POST) {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likeCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+      this._state.dialogsPage.newMessageBody = action.body;
+      this._callSubscriber(this._state);
+    } else if (action.type === SEND_MESSAGE) {
+      let body = this._state.dialogsPage.newMessageBody;
+      this._state.dialogsPage.newMessageBody = '';
+      this._state.dialogsPage.messages.push({id: 4, message: body});
+      this._callSubscriber(this._state);
+    }
+  }
 }
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likeCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
+export const addPostActionCreator = () =>  ({type: ADD_POST,});
+export const updateNewPostTextActionCreator = (text) =>  ({ type: UPDATE_NEW_POST_TEXT, newText: text,});
 
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
+export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body});
 
-export default state;
+export default store;
+window.store = store;
