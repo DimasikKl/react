d7e4af07006c8f932f0b9610173af794aed6daa7
Клёп Дмитрий <dmitiykloyp@gmail.com>
2021-11-03 00:56:09 +0300

    Подключил библиотеку axios сделал первый get  запрос

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-stor";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
-let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
-    </BrowserRouter>, document.getElementById('root'));
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>, document.getElementById('root'));
